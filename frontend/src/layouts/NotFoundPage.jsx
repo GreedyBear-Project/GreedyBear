51a1eadcbbaf422b72fbe2f5ec6b9323377f6658
suvani-ctrl <suvanibasnet8@gmail.com>
2026-05-21 13:20:02 -0400

    Improve 404 NotFoundPage UI and show failed path. Closes #1351

--- frontend/src/layouts/NotFoundPage.jsx
+++ frontend/src/layouts/NotFoundPage.jsx
@@ -1,37 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ButtonGroup, Container,Button } from "reactstrap";
-import { useLocation } from 'react-router'
-
+import { ButtonGroup, Container, Button } from "reactstrap";
+import { useLocation } from "react-router";
 
 function NotFoundPage() {
-  console.debug("NotFoundPage rendered!");
   const location = useLocation();
-  console.log("The current location is",location);
   return (
-    <Container 
-      className="d-flex flex-column align-items-center justify-content-center text-center" 
-      style={{ minHeight: '80vh' }}
+    <Container
+      className="d-flex flex-column align-items-center justify-content-center text-center"
+      style={{ minHeight: "80vh" }}
     >
-      <h1 className = "display-1 text-danger font-weight-bold">404</h1>
+      <h1 className="display-1 text-danger font-weight-bold">404</h1>
       <h2 className="mb-3">Page Not Found</h2>
 
       <p className="text-muted mb-4">
-        The requested path <code className="text-light">{location.pathname}
-        </code> does not exists or has been moved.
+        The requested path{" "}
+        <code className="text-white font-weight-bold fs-5">
+          {location.pathname}
+        </code>{" "}
+        does not exist or has been moved.
       </p>
 
       <div className="mb-4">
-        <p mb-2 font-weight-bold text-secondary>
-         Return to  
-        </p>
-    
-        <ButtonGroup>
-          <Button tag={Link} to="/" color="primary" outline> Home </Button>
-          <Button tag={Link} to="/feeds" color="primary" outline> Feeds </Button>
-          <Button tag={Link} to="/dashboard" color="primary" outline> Dashboard </Button>
-        </ButtonGroup>
+        <p className="text-light font-weight-bold">Return to</p>
 
+        <ButtonGroup>
+          <Button tag={Link} to="/" color="primary" outline>
+            {" "}
+            Home{" "}
+          </Button>
+          <Button tag={Link} to="/feeds" color="primary" outline>
+            {" "}
+            Feeds{" "}
+          </Button>
+          <Button tag={Link} to="/dashboard" color="primary" outline>
+            {" "}
+            Dashboard{" "}
+          </Button>
+        </ButtonGroup>
       </div>
     </Container>
   );
