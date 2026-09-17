a54c4534b8403094fe7edfdd30f869d7391465e4
tim <46972822+regulartim@users.noreply.github.com>
2026-09-17 19:06:35 +0200

    add credentials to raw_id_fields in CowrieSessionModelAdmin

--- greedybear/admin.py
+++ greedybear/admin.py
@@ -162,7 +162,7 @@ class CowrieSessionModelAdmin(GreedyBearModelAdmin):
     ]
     search_fields = ["source__name"]
     search_help_text = "search for the IP address source"
-    raw_id_fields = ["source", "commands"]
+    raw_id_fields = ["source", "commands", "credentials"]
     list_filter = ["login_attempt", "command_execution"]
 
     def credential_list(self, session):
